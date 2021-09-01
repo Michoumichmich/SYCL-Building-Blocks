2af41b44247d9390e744f78493bdc178ce39b0af
Michel MIGDAL <michel.migdal@ensiie.fr>
2021-09-01 17:32:03 +0200

    More intrinsics

--- include/intrinsics.hpp
+++ include/intrinsics.hpp
@@ -29,61 +29,100 @@ namespace internal {
 
 }
 
-
+/**
+ * For NVPTX macro @see https://intel.github.io/llvm-docs/clang_doxygen/NVPTX_8cpp_source.html
+ * For inline PTX @see https://docs.nvidia.com/cuda/inline-ptx-assembly/index.html
+ * For PTX ISA doc @see https://docs.nvidia.com/cuda/parallel-thread-execution/index.html#integer-arithmetic-instructions-dp4a
+ */
 namespace sycl::ext {
+
+
+    using sycl::ext::intel::ctz;
+
+    /**
+     * @see https://docs.nvidia.com/cuda/cuda-math-api/group__CUDA__MATH__INTRINSIC__INT.html#group__CUDA__MATH__INTRINSIC__INT_1gf939c350eafa2f13d64e278549d3a8aa
+     */
     inline uint32_t funnelshift_l(uint32_t lo, uint32_t hi, uint32_t shift) {
         if (shift == 0) return hi; // To avoid shifting by 32
         return (hi << (uint) (shift % 31)) | (lo >> (uint) (32 - (shift % 31)));
     }
 
+    /**
+     * @see https://docs.nvidia.com/cuda/cuda-math-api/group__CUDA__MATH__INTRINSIC__INT.html#group__CUDA__MATH__INTRINSIC__INT_1g125eeef4993d16dc8d679b239460fc34
+     */
     inline uint32_t funnelshift_r(uint32_t lo, uint32_t hi, uint32_t shift) {
         if (shift == 0) return lo; // To avoid shifting by 32
         return (lo >> shift % 31) | (hi << (32 - (shift % 31)));
     }
 
-    inline uint32_t brev32(uint32_t num) {
-        const int size = sizeof(uint32_t) * 8;
+
+    /**
+     * Prefetches the data to the L1 cache on the CUDA Back-end, else is a no-op.
+     * @see https://docs.nvidia.com/cuda/parallel-thread-execution/index.html#data-movement-and-conversion-instructions-prefetch-prefetchu
+     * @tparam T pointed type
+     * @param ptr address to prefetch
+     */
+    template<typename T>
+    void static inline prefetch(const T *ptr) {
+#if defined (__NVPTX__) && defined(__SYCL_DEVICE_ONLY__)
+        asm("prefetch.L1 [%0];" :  : "l"(ptr));
+#else
+        (void)ptr;
+#endif
+    }
+
+
+    /**
+     * Bit reversal
+     */
+    static inline uint32_t brev32(uint32_t num) {
         auto reverse_num = uint32_t(0);
+#if defined (__NVPTX__) && defined(__SYCL_DEVICE_ONLY__)
+        asm("brev.b32 %0, %1;" : "=r"(reverse_num) : "r"(num));
+#else
+        const int size = sizeof(uint32_t) * 8;
         for (int i = 0; i < size; i++) {
-            reverse_num |= uint32_t((num & (uint32_t(1) << i)) != 0) << ((size - 1) - i); // branchless?
-            //if ((num & (1 << i)))
-            //    reverse_num |= 1 << ((size - 1) - i);
+            reverse_num |= uint32_t((num & (uint32_t(1) << i)) != 0) << ((size - 1) - i);
         }
+#endif
         return reverse_num;
     }
 
-    inline uint64_t brev64(uint64_t num) {
-        const int size = sizeof(uint64_t) * 8;
+    /**
+     * Bit reversal
+     */
+    static inline uint64_t brev64(uint64_t num) {
         auto reverse_num = uint64_t(0);
+#if defined (__NVPTX__) && defined(__SYCL_DEVICE_ONLY__)
+        asm("brev.b64 %0, %1;" : "=l"(reverse_num) : "l"(num));
+#else
+        const int size = sizeof(uint64_t) * 8;
         for (int i = 0; i < size; i++) {
-            reverse_num |= uint64_t((num & (uint64_t(1) << i)) != 0) << ((size - 1) - i); // branchless?
-            //if ((num & (1ul << i)))
-            //    reverse_num |= 1ul << ((size - 1) - i);
+            reverse_num |= uint64_t((num & (uint64_t(1) << i)) != 0) << ((size - 1) - i);
         }
+#endif
         return reverse_num;
     }
 
 
     template<typename func>
-    static uint32_t predicate_to_mask(const sycl::sub_group &sg, func &&predicate) {
-        size_t group_count = sg.get_local_range().size();
+    static inline uint32_t predicate_to_mask(const sycl::sub_group &sg, func &&predicate) {
+        uint32_t group_count = sg.get_local_range().size();
         uint32_t out = 0;
         for (size_t gr = 0; gr < group_count; ++gr) {
-            if (predicate(gr)) {
-                out |= uint32_t(1) << gr;
-            }
+            out |= uint32_t(predicate(gr) ? 1 : 0) << gr;
         }
         return out;
     }
 
-    uint32_t ballot(const sycl::sub_group &sg, int predicate) {
+    static inline uint32_t ballot(const sycl::sub_group &sg, int predicate) {
         uint32_t local_val = (predicate ? 1u : 0u) << (sg.get_local_linear_id());
         return sycl::reduce_over_group(sg, local_val, sycl::plus<>());
     }
 
 
     template<typename T>
-    uint32_t match_any(const sycl::sub_group &sg, T val) {
+    static inline uint32_t match_any(const sycl::sub_group &sg, T val) {
         size_t local_range = sg.get_local_range().size();
         uint32_t found = 0;
         for (uint32_t i = 0; i < local_range; ++i) {
@@ -95,11 +134,11 @@ namespace sycl::ext {
 
 
     template<typename T>
-    bool match_all(const sycl::sub_group &sg, uint32_t mask, T val) {
-        if (mask == 0 || sycl::clz(mask) >= 8 * sizeof(mask)) return false;
-        size_t last_work_item_id = 8 * sizeof(mask) - 1 - sycl::clz(mask);
-        if (last_work_item_id > sg.get_local_range().size()) return false;
-        const T from_others = sycl::select_from_group(sg, val, last_work_item_id);
+    static inline bool match_all(const sycl::sub_group &sg, uint32_t mask, T val) {
+        if (mask == 0) return false;
+        size_t first_work_item_id = sycl::ext::intel::ctz(mask);
+        if (first_work_item_id > sg.get_local_range().size()) return false;
+        const T from_others = sycl::select_from_group(sg, val, first_work_item_id);
         return mask == (ballot(sg, val == from_others) & mask);
     }
 
@@ -146,19 +185,23 @@ void check_builtins(sycl::queue q) {
         check_builtins();
         if (is_host) return;
         auto sg = it.get_sub_group();
+        auto mask_all = sycl::ext::predicate_to_mask(sg, [&](size_t i) { return true; }); // Select threads where tid is even
+        auto mask_even = sycl::ext::predicate_to_mask(sg, [&](size_t i) { return i % 2 == 0; }); // Select threads where tid is even
+        auto mask_odd = sycl::ext::predicate_to_mask(sg, [&](size_t i) { return i % 2 != 0; }); // Select threads where tid is even
 
         SYCL_ASSERT(sycl::popcount(sycl::ext::ballot(sg, 1)) == sg.get_local_range().size())
         SYCL_ASSERT(0 == sycl::ext::broadcast_leader(sg, sg.get_local_linear_id()))
         SYCL_ASSERT(sycl::ext::match_all(sg, 1, 1))
-        SYCL_ASSERT(sycl::ext::match_all(sg, 0xFFFFFFFF, 0xDEADBEEF))
-        SYCL_ASSERT(!sycl::ext::match_all(sg, 0xFFFFFFFF, it.get_local_linear_id()))
+        SYCL_ASSERT(sycl::ext::match_all(sg, mask_all, 0xDEADBEEF))
+        SYCL_ASSERT(!sycl::ext::match_all(sg, mask_all, it.get_local_linear_id()))
 
-        auto mask_even = sycl::ext::predicate_to_mask(sg, [&](size_t i) { return i % 2 == 0; }); // Select threads where tid is even
-        auto mask_odd = sycl::ext::predicate_to_mask(sg, [&](size_t i) { return i % 2 != 0; }); // Select threads where tid is even
         SYCL_ASSERT(sycl::ext::match_all(sg, mask_even, it.get_local_linear_id() % 2))
 
         uint32_t expected = (it.get_local_linear_id() % 2 == 0) ? mask_even : mask_odd;
         SYCL_ASSERT(expected == sycl::ext::match_any(sg, it.get_local_linear_id() % 2 == 0))
+
+        int val;
+        sycl::ext::prefetch(&val);
 
 
     }).wait_and_throw();
